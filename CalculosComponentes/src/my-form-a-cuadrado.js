65b0a070e5ee5f706d227644bf792fc94579cc56
S-117 <rodrigobarreraangelromero@gmail.com>
2023-10-18 11:42:21 -0500

    Componente Cuadrado.
    Componente cuadrado funcional.

--- CalculosComponentes/src/my-form-a-cuadrado.js
+++ CalculosComponentes/src/my-form-a-cuadrado.js
@@ -5,21 +5,36 @@ export class MyFormACuadrado extends LitElement{
     static get styles(){
         return [myFormACuadradoStyle]
     }
+    static properties = {
+        lado: {
+            type: Number
+        }
+    }
+    constructor(){
+        super();
+        this.lado = 0;
+    }
     render(){
         return html`
         <div class="contenedor-principal">
             <h1>Calcular Area Del Cuadrado</h1>
             <div class="contenedor-formulario">
-                <form method="POST">
+                <form>
                     <img src="https://static.vecteezy.com/system/resources/previews/003/123/180/non_2x/saber-neon-glow-color-flowing-square-background-free-photo.jpg" alt="Cuadrado">
                     <label>Ingrese el lado del cuadrado:</label>
-                    <input type="text" name="lado" id="ladoCua">
-                    <button>Calcular</button>
+                    <input type="number" name="lado" id="ladoCua">
+                    <button type="button" @click=${this.calcularArea}>Calcular</button>
+                    <p>El resultado de la operacion es: ${this.lado}</p>
                 </form>
             </div>
         </div>
-        `
+        `;
     }
+    calcularArea(){
+        const l = this.shadowRoot.getElementById("ladoCua").value;
+        const resultado = l*l;
+        this.lado = resultado;
+    }            
 }
 
 customElements.define('my-form-a-cuadrado', MyFormACuadrado)
